57194340992995d80ec46c90f48386e7ae1e6b96
StudentMe2712 <egorclanwar980@gmail.com>
2026-08-30 10:45:09 +0500

    fix(NexusCommander): Fix MultiDataTrigger XAML exception and ensure stable window launch on dotnet run

--- projects/ZenithCommander/App.xaml.cs
+++ projects/ZenithCommander/App.xaml.cs
@@ -1,7 +1,29 @@
+using System;
+using System.IO;
 using System.Windows;
 
 namespace NexusCommander;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+        {
+            var ex = args.ExceptionObject as Exception;
+            var msg = $"[AppDomain Unhandled] {ex?.ToString() ?? args.ExceptionObject?.ToString()}";
+            Console.WriteLine(msg);
+            File.WriteAllText("startup_error.log", msg);
+        };
+
+        DispatcherUnhandledException += (sender, args) =>
+        {
+            var msg = $"[Dispatcher Unhandled] {args.Exception}";
+            Console.WriteLine(msg);
+            File.WriteAllText("startup_error.log", msg);
+            args.Handled = false;
+        };
+
+        base.OnStartup(e);
+    }
 }
